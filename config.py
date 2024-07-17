0ac6a787392a4df954cc523519a5817e27529876
HYPER-prog-deal <hypergamer6336@gmail.com>
2024-07-17 22:43:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7023476835:AAFCZO_leBiIDcEXclf1BfeQd14DqkkNJDk")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("HYPER_OMI1")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6682821934")
 
 
 
